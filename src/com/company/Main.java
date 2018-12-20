e780a69e1a8a7c0694718c59e727d0e34ad59eb8
edson <edsaraujoc@gmail.com>
2018-12-20 13:34:59 -0200

    Implementacao do Algoritmo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+         double precos[] = new double[5];
+         precos[0] = 1000000;
+         precos[1] = 46000;
+         precos[2] = 16000;
+         precos[3] = 46000;
+         precos[4] = 17000;
+
+         int maisBarato = 0;
+
+         for(int atual = 0; atual <= 4; atual++) {
+             if(precos[atual] < precos[maisBarato]) {
+                 maisBarato = atual;
+             }
+         }
+
+         System.out.println("O carro mais barato custa " + precos[maisBarato]);
+
     }
 }
