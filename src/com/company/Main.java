94c4b98e66e577fdd0e6a4ccf60fac9326cc2eda
edson <edsaraujoc@gmail.com>
2018-12-20 15:57:09 -0200

    Refatoracao do tamanho do Array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,18 +10,18 @@ public class Main {
                 new Produto("Brasilia", 16000),
                 new Produto("Smart", 46000)
         };
-        int maisBarato = buscaMenor(produtos);
 
+        int maisBarato = buscaMenor(produtos, 4);
         System.out.println("O Veiculo mais barato e: " + produtos[maisBarato].getNome());
         System.out.println("O valor e R$:" + produtos[maisBarato].getPreco());
 
     }
 
-    public static int buscaMenor(Produto[] produtos) {
+    public static int buscaMenor(Produto[] produtos, int termino) {
         int maisBarato = 0;
-        int termino = produtos.length - 1;
+        //int termino = produtos.length - 1;
 
-        for(int i = 0; i <= termino; i++) {
+        for(int i = 0; i < termino; i++) {
             if(produtos[i].getPreco() < produtos[maisBarato].getPreco()) {
                 maisBarato = i;
             }
